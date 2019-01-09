2ecfe0c7d17468a78d297a783c62701bfed2f1a4
asvirin <45921969+asvirin@users.noreply.github.com>
2019-01-09 22:07:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from instabot import Bot
 from dotenv import load_dotenv
 from os import listdir
+load_dotenv()
 
 def send_photos(dirfile, filename):
-    load_dotenv()
     username = os.getenv("username")
     password = os.getenv("password")
     example_caption = os.getenv("caption")
@@ -15,7 +15,6 @@ def send_photos(dirfile, filename):
     bot.upload_photo(pic, example_caption)
     
 if __name__ == '__main__':
-    load_dotenv()
     dirfile = os.getenv("dirfile")
     photos_list = listdir(dirfile)
     for filename in photos_list:
