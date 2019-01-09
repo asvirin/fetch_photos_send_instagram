9d7e8b5bcf3fc2013eb6379088ff037e11c5632e
asvirin <45921969+asvirin@users.noreply.github.com>
2019-01-09 22:27:23 +0300

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -8,13 +8,13 @@ def fetch_spacex():
     launches_data = r.json()
 
     for flight_number, flight_info in enumerate(launches_data):
-        for images_number, images_url  in enumerate(flight_info['links']['flickr_images']):
-            filename = 'spasex' + str(flight_number)+str(images_number) + '.jpg'
-                get_images_spacex(images_url, filename)
+        for image_number, image_url  in enumerate(flight_info['links']['flickr_images']):
+            filename = 'spacex' + str(flight_number)+str(image_number) + '.jpg'
+                get_images_spacex(image_url, filename)
                 
 def get_images_spacex(url, filename):
     r = requests.get(url)
-    dirfile = os.getenv("dirfile")
-    fullpath = dirfile + filename
+    folder_with_pics = os.getenv("folder_with_pics")
+    fullpath = folder_with_pics + filename
     with open(fullpath, 'wb') as f:  
         f.write(r.content)
