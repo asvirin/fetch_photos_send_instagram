cbbf8ba4aa386dcd48d938ac3461c9404df4a810
asvirin <45921969+asvirin@users.noreply.github.com>
2019-01-11 23:50:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from dotenv import load_dotenv
 from os import listdir
 load_dotenv()
 
-def main(folder_with_pics, filename):
+def main():
+    folder_with_pics = os.getenv("folder_with_pics")
+    photos_list = listdir(folder_with_pics)
     username = os.getenv("username")
     password = os.getenv("password")
     example_caption = os.getenv("caption")
-
     bot = Bot()
     bot.login(username, password)
-
-    pic = folder_with_pics + filename
-    bot.upload_photo(pic, example_caption)
+    
+    for filename in photos_list:
+        pic = folder_with_pics + filename
+        bot.upload_photo(pic, example_caption)
     
 if __name__ == '__main__':
-    folder_with_pics = os.getenv("folder_with_pics")
-    photos_list = listdir(folder_with_pics)
-    for filename in photos_list:
-        main(folder_with_pics, filename)
+    main()
